34fe085f645a439b274f35359a61cd3cc74a1981
aboratorium <100868994+aboratorium@users.noreply.github.com>
2025-05-12 22:29:08 +0200

    Update load_data.py
    🔧 Updated load_data.py: added get_cost_benchmarks() function for cost assumptions per development type.

--- utils/load_data.py
+++ utils/load_data.py
@@ -4,3 +4,22 @@ def load_benchmark_data():
         "Hospitality": 15.2,
         "Mixed-Use": 16.8
     }
+
+def get_cost_benchmarks():
+    return {
+        "Residential": {
+            "construction_cost_per_m2": 1000,
+            "marketing_cost_pct": 5,
+            "other_costs_pct": 10
+        },
+        "Hospitality": {
+            "construction_cost_per_m2": 1200,
+            "marketing_cost_pct": 6,
+            "other_costs_pct": 12
+        },
+        "Mixed-Use": {
+            "construction_cost_per_m2": 1100,
+            "marketing_cost_pct": 5.5,
+            "other_costs_pct": 11
+        }
+    }
